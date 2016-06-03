8c6bcc6a9f317a7fd1d655fbc3a685136f46d49c
Finn Poppinga <finno.ger@gmail.com>
2016-06-03 14:03:42 +0200

    rendering vertices

--- cg.jl
+++ cg.jl
@@ -1,64 +1,76 @@
-import Base.+;
-import Base.-;
-import Base.*;
+module CG
+  import Base.+;
+  import Base.-;
+  import Base.*;
 
-# Basic types
-type Vec4{T<:Number}
-  e1::T
-  e2::T
-  e3::T
-  e4::T
-end
+  export Vec4, Vec4f, Mat4, Mat4f, Object;
 
-type Mat4{T<:Vec4}
-  v1::T
-  v2::T
-  v3::T
-  v4::T
-end
+  # Basic types
+  type Vec4{T<:Number}
+    e1::T
+    e2::T
+    e3::T
+    e4::T
+  end
 
-#typedefs
-Vec4f = Vec4{Float32};
-Mat4f = Mat4{Vec4f};
+  type Mat4{T<:Vec4}
+    v1::T
+    v2::T
+    v3::T
+    v4::T
+  end
 
-##operators
-# 1) vector addition
-+ (v1::Vec4, v2::Vec4) =
-begin
-  return Vec4f(
-    v1.e1 + v2.e1,
-    v1.e2 + v2.e2,
-    v1.e3 + v2.e3,
-    v1.e4 + v2.e4
-  );
-end
+  #typedefs
+  Vec4f = Vec4{Float32};
+  Mat4f = Mat4{Vec4f};
 
--(v1::Vec4) =
-begin
-  return Vec4(-v1.e1, -v1.e2, -v1.e3, -v1.e4);
-end
+  ##operators
+  # 1) vector addition
+  + (v1::Vec4, v2::Vec4) =
+  begin
+    return Vec4f(
+      v1.e1 + v2.e1,
+      v1.e2 + v2.e2,
+      v1.e3 + v2.e3,
+      v1.e4 + v2.e4
+    );
+  end
 
--(v1::Vec4, v2::Vec4) =
-begin
-  return v1 + - v2;
-end
+  -(v1::Vec4) =
+  begin
+    return Vec4(-v1.e1, -v1.e2, -v1.e3, -v1.e4);
+  end
 
-*(a::Number, v::Vec4) =
-begin
-  return Vec4f(a * v.e1, a * v.e2, a * v.e3, a * v.e4);
-end
+  -(v1::Vec4, v2::Vec4) =
+  begin
+    return v1 + - v2;
+  end
 
-*(v1::Vec4, v2::Vec4) =
-begin
-  return v1.e1 * v2.e1 + v1.e2 * v2.e2 + v1.e3 * v2.e3 + v1.e4 * v2.e4;
-end
-# 2) matrix operations
-*(m::Mat4, v::Vec4) =
-begin
-  return Vec4f(
-    m.v1 * v,
-    m.v2 * v,
-    m.v3 * v,
-    m.v4 * v
-  );
+  *(a::Number, v::Vec4) =
+  begin
+    return Vec4f(a * v.e1, a * v.e2, a * v.e3, a * v.e4);
+  end
+
+  # scalar multiplication
+  *(v1::Vec4, v2::Vec4) =
+  begin
+    return v1.e1 * v2.e1 + v1.e2 * v2.e2 + v1.e3 * v2.e3 + v1.e4 * v2.e4;
+  end
+  # 2) matrix operations
+  *(m::Mat4, v::Vec4) =
+  begin
+    return Vec4f(
+      m.v1 * v,
+      m.v2 * v,
+      m.v3 * v,
+      m.v4 * v
+    );
+  end
+
+  ## Object
+  type Object{T<:Vec4}
+    vertices::Vector{T}
+    Object(x::Vector{T}) = new(x)
+    Object(x::T...) = new(collect(T, x))
+  end
 end
--- ex1.jl
+++ ex1.jl
@@ -1,14 +1,36 @@
 include("cg.jl")
+using CG;
+include("render.jl");
 
-v1 = Vec4f(1.0, 0.0, 0.0, 0.0);
+v1 = Vec4f(1, 0, 0, 0);
 v2 = Vec4f(0.0, 42.0, 0.0, 0.0);
 
-print(v1 - v2);
+println(v1 - v2);
 
 m1 = Mat4f(Vec4f(1.0, 0.0, 0.0, 0.0),
            Vec4f(0.0, 1.0, 0.0, 0.0),
            Vec4f(0.0, 0.0, 1.0, 0.0),
            Vec4f(0.0, 0.0, 0.0, 1.0));
 
-print(2 * (m1 * v1));
-print(m1 * v2);
+println(2 * (m1 * v1));
+println(m1 * v2);
+
+
+v1 = Vec4f(1, 0, 0, 1);
+v2 = Vec4f(0, 0, 0, 1);
+v3 = Vec4f(0, 0, 1, 1);
+
+triangle = Object{Vec4f}(v1, v2, v3);
+
+house = Object{Vec4f}(Vec4f(-1, -1, 0, 1),
+Vec4f(1, -1, 0, 1),
+Vec4f(-1, 1, 0, 1),
+Vec4f(0, 2, 0, 1),
+Vec4f(1, 1, 0, 1),
+Vec4f(1, 1, 0, 1),
+Vec4f(-1, -1, 0, 1),
+Vec4f(-1, 1, 0, 1),
+Vec4f(1, 1, 0, 1),
+Vec4f(1, -1, 0, 1))
+
+render(house, figAxis=[-2, 2, -2, 2]);
